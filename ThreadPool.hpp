633e5ee45612609e5c07a63ecd917aebd396aa19
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2023-03-03 14:24:14 +0800

    format

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -1,31 +1,36 @@
-#include<cstdint>
-#include<mutex>
-#include<thread>
-#include<functional>
-#include<future>
-#include<condition_variable>
-#include<iostream>
+#include <cstdint>
+#include <mutex>
+#include <thread>
+#include <functional>
+#include <future>
+#include <condition_variable>
+#include <iostream>
 #include "queue_wrapper.hpp"
 using namespace std;
 
-class ThreadPool{
-    private:
+class ThreadPool
+{
+private:
     vector<thread> workers;
-    Queue< function<void()> > jobs;
+    Queue<function<void()>> jobs;
     mutex m;
     condition_variable cv;
     bool stop;
-    public:
-    ThreadPool(int=4);
-    ~ThreadPool();
-    template<class F,class ...Args>
-    void submit(F&& f, Args&& ... args);
-    void shutdown();
 
+public:
+    ThreadPool(int = 4);
+    ~ThreadPool();
+    template <class F, class... Args>
+    void submit(F &&f, Args &&...args);
+    void shutdown();
 };
-ThreadPool::ThreadPool(int max_workers):stop(false){
-    for(int i=0;i<max_workers;i++){
-        thread worker([&]{
+
+ThreadPool::ThreadPool(int max_workers) : stop(false)
+{
+    for (int i = 0; i < max_workers; i++)
+    {
+        thread worker([&]
+                      {
         while(1){
             unique_lock<mutex> lk(m);
             cv.wait(lk,[&]{return stop||!jobs.empty();});
@@ -34,37 +39,41 @@ ThreadPool::ThreadPool(int max_workers):stop(false){
             jobs.pop();
             lk.unlock();
             run();
-        }
-        });
+        } });
         workers.emplace_back(move(worker));
     }
-
 }
-template<class F,class ...Args>
-void ThreadPool::submit(F&& f,Args&&... args){
-    auto func=bind(forward<F>(f),forward<Args>(args)...);
+template <class F, class... Args>
+void ThreadPool::submit(F &&f, Args &&...args)
+{
+    auto func = bind(forward<F>(f), forward<Args>(args)...);
     {
         unique_lock<mutex> lk(m);
-        if(stop){
-            cout<<"ThreadPool has stopped.\n";
+        if (stop)
+        {
+            cout << "ThreadPool has stopped.\n";
         }
-        jobs.push([=](){func();});
+        jobs.push([=]()
+                  { func(); });
     }
-    
+
     cv.notify_one();
 }
-void ThreadPool::shutdown(){
+void ThreadPool::shutdown()
+{
     {
         unique_lock<mutex> lk(m);
-        stop=1;
+        stop = 1;
     }
     cv.notify_all();
-    for(thread& worker:workers){
-        if(worker.joinable())
+    for (thread &worker : workers)
+    {
+        if (worker.joinable())
             worker.join();
     }
 }
-ThreadPool::~ThreadPool(){
-    if(!stop)
+ThreadPool::~ThreadPool()
+{
+    if (!stop)
         shutdown();
 }
